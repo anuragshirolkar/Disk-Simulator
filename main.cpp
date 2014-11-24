f86a54920f79ce8d47761f9f472e5ff1ff6d0ad2
Anurag Shirolkar <ashirolkar47@gmail.com>
2014-11-24 15:33:14 +0530

    added main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
 #include "DiskController.hpp"
 #include <iostream>
 #include <cstdio>
+#include <string.h>
+
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	DiskController disk;
+	int timer=0;
+	DiskController disk(&timer);
+
 	char c[512];
-	disk.read_data(98354,c,1);
+	strcpy(c,"Hello I am an Indian");
+	int next_op = 0;
+	cin >> next_op;
+	for(timer = 0; timer < 1000000; timer++) {
+		if(next_op == timer){
+			int addr, rw;
+			cin >> addr >> rw;
+			//cout << addr << " " << rw << endl;
+			//if(rw == 0) cerr << rw << endl;
+			if(rw == 0) disk.read_data(addr,c,timer);
+			if(rw == 1) {
+				cin.getline(c, 512);
+				cin.getline(c, 512);
+				disk.write_data(addr,c,timer);
+			}
+			cin >> next_op;
+			if(next_op == -1) {
+				cerr << timer << endl << "======================" << endl;
+				disk.print_stats(); 
+				cin >> next_op;
+			}
+		}
+		//cout << timer << endl;
+		//cerr << timer << endl;
+		disk.update();
+		//cout << timer << endl;
+	}
 	return 0;
 
 
